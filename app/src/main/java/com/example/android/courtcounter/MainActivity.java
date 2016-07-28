2408e00314bd39f3981804912f37164898fa6f25
chkumar <himakiran@gmail.com>
2016-07-28 23:05:05 +0530

    Coded functions to act on button press and display score.. App performing properly.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int Score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayScore(Score);
+
+    }
+
+
+    private void displayScore(int score) {
+        TextView t = (TextView) findViewById(R.id.scoreBoard);
+        t.setText("" + score);
+    }
+
+    public void threePointer(View view) {
+        Score += 3;displayScore(Score);
+    }
+
+    public void twopointer(View view) {
+        Score += 2;displayScore(Score);
+    }
+
+    public void freeThrow(View view) {
+        Score += 1;displayScore(Score);
     }
 }

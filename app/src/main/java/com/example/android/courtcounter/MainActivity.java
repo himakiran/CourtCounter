d376150b4149f17b811f4c40d76658bd5a903f0a
chkumar <himakiran@gmail.com>
2016-07-31 03:45:46 +0530

    Made the app pretty
    Changed the xml layout file to get the UI right

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -15,27 +15,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     /**
      * Displays the given score for Team A.
      */
     public int scoreTeamA = 0;
     public int scoreTeamB = 0;
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-    public void addThreeforTeamA(View view)
-    {
-        scoreTeamA += 3;displayForTeamA(scoreTeamA);
+
+    public void addThreeforTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
     }
-    public void addTwoforTeamA(View view)
-    {
-        scoreTeamA += 2;displayForTeamA(scoreTeamA);
+
+    public void addTwoforTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
-    public void addOneforTeamA(View view)
-    {
-        scoreTeamA += 1;displayForTeamA(scoreTeamA);
+
+    public void addOneforTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
     }
+
     /**
      * Displays the given score for Team B.
      */
@@ -43,25 +49,28 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
-    public void addThreeforTeamB(View view)
-    {
-        scoreTeamB += 3;displayForTeamB(scoreTeamB);
+
+    public void addThreeforTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
     }
-    public void addTwoforTeamB(View view)
-    {
-        scoreTeamB += 2;displayForTeamB(scoreTeamB);
+
+    public void addTwoforTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
     }
-    public void addOneforTeamB(View view)
-    {
-        scoreTeamB += 1;displayForTeamB(scoreTeamB);
+
+    public void addOneforTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
     }
+
     /**
      * Resets the scores of both Team A and Team B
      */
-    public void doReset(View view)
-    {
+    public void doReset(View view) {
         scoreTeamA = 0;
-        scoreTeamB =  0;
+        scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
     }
